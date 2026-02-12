64c5657d7f50342b497d83740c5e0003fc1f926a
Syon <17407syon@gmail.com>
2026-02-13 00:56:41 +0530

    Fix after deploying to PythonAnywhere

--- flask_app.py
+++ flask_app.py
@@ -1,15 +1,15 @@
 from flask import Flask, request, jsonify, render_template
 import sqlite3
+import os
+
+#BASE_DIR = "/home/Cracked/UPI-Tracker"
+DB_PATH = os.path.join(BASE_DIR, "database.db")
 
 app = Flask(__name__)
+with sqlite3.connect(DB_PATH) as conn:
+    conn.execute('''CREATE TABLE IF NOT EXISTS transactions
+               (id INTEGER PRIMARY KEY AUTOINCREMENT, type TEXT, amount REAL, person TEXT, timestamp TEXT)''')
 
-conn = sqlite3.connect('database.db')
-cur = conn.cursor()
-cur.execute('''CREATE TABLE IF NOT EXISTS transactions
-               (id INTEGER PRIMARY KEY AUTOINCREMENT, type TEXT, amount REAL, person TEXT, timestamp DATETIME)''')
-conn.close()
-
-LOG_FILE = "sms_log.txt"
 
 def parse_sms_data(data, time):
     value = {}
@@ -20,32 +20,27 @@ def parse_sms_data(data, time):
 
     value['amount'] = float(".".join(data[0].split(".")[-2:]))
     value['person'] = " ".join(data[2].split(" ")[1:])
-    date = data[3].split(" ")[1]
     value['timestamp'] = time
 
     return value
 
-@app.route('/')
-def index():
-    with open(LOG_FILE, "r") as f:
-        lines = f.readlines()
-    return render_template('index.html', values=lines)
 
 @app.route('/sms', methods=['POST'])
 def receive_sms():
-    conn = sqlite3.connect('database.db')
+    conn = sqlite3.connect(DB_PATH)
     cur = conn.cursor()
     data = request.json
     text = data.get('text', 'Unknown')
     time = data.get('time', 'Unknown')
     values = parse_sms_data(text, time)
-    
+
     cur.execute("INSERT INTO transactions (type, amount, person, timestamp) VALUES (?, ?, ?, ?)",
                 (values['type'], values['amount'], values['person'], values['timestamp']))
     conn.commit()
     conn.close()
 
     return jsonify({"status": "logged"}), 200
+
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
