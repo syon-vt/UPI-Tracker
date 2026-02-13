f905c08efe2875bd14a967a4045acb5a8ce83045
Syon <17407syon@gmail.com>
2026-02-13 14:23:30 +0530

    fixed credit and added test home page

--- flask_app.py
+++ flask_app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify, render_template
 import sqlite3
 import os
 
-#BASE_DIR = "/home/Cracked/UPI-Tracker"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DB_PATH = os.path.join(BASE_DIR, "database.db")
 
 app = Flask(__name__)
@@ -15,11 +15,15 @@ def parse_sms_data(data, time):
     value = {}
     data = data.split("\n")
 
-    if data[0].startswith("Sent"): value["type"] = 'debit'
-    elif data[0].startswith("Credit"): value["type"] = 'credit'
+    if data[0].startswith("Sent"):
+        value["type"] = 'debit'
+        value['amount'] = float(".".join(data[0].split(".")[-2:]))
+        value['person'] = " ".join(data[2].split(" ")[1:])
 
-    value['amount'] = float(".".join(data[0].split(".")[-2:]))
-    value['person'] = " ".join(data[2].split(" ")[1:])
+    elif data[0].startswith("Credit"): 
+        value["type"] = 'credit'
+        value['amount'] = float(".".join(data[1].split()[0].split(".")[-2:]))
+        value['person'] = data[1].split()[-3]
     value['timestamp'] = time
 
     return value
@@ -27,20 +31,27 @@ def parse_sms_data(data, time):
 
 @app.route('/sms', methods=['POST'])
 def receive_sms():
-    conn = sqlite3.connect(DB_PATH)
-    cur = conn.cursor()
     data = request.json
     text = data.get('text', 'Unknown')
     time = data.get('time', 'Unknown')
     values = parse_sms_data(text, time)
 
-    cur.execute("INSERT INTO transactions (type, amount, person, timestamp) VALUES (?, ?, ?, ?)",
-                (values['type'], values['amount'], values['person'], values['timestamp']))
-    conn.commit()
-    conn.close()
+    with sqlite3.connect(DB_PATH) as conn:
+        cur = conn.cursor()
+        cur.execute("INSERT INTO transactions (type, amount, person, timestamp) VALUES (?, ?, ?, ?)",
+                    (values['type'], values['amount'], values['person'], values['timestamp']))
+    
 
     return jsonify({"status": "logged"}), 200
 
 
-if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+@app.route('/')
+def index():
+    with sqlite3.connect(DB_PATH) as conn:
+        cur = conn.cursor()
+        cur.execute("SELECT type, amount, person, timestamp FROM transactions")
+        transactions = cur.fetchall()
+    return render_template('index.html', transactions=transactions)
+
+"""if __name__ == '__main__':
+    app.run(port=5000, debug=True)"""
